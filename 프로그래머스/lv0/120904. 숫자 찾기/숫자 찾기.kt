4dfebb54f935e593b31a4ffc8a4aa326fd1d85b3
다을짱 <139089298+ouowinnie@users.noreply.github.com>
2023-08-07 13:39:33 +0900

    [level 0] Title: 숫자 찾기, Time: 14.09 ms, Memory: 60.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.kt
+++ 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.kt
@@ -1,7 +1,6 @@
 class Solution {
     fun solution(num: Int, k: Int): Int {
-        var numArray = num.toString().map { it.toString().toInt() }.toIntArray()
-        var answer = numArray.indexOf(k)
-        return if (answer != -1 && numArray[answer] == k) answer +1 else -1
+        var answer: Int = ("-$num").indexOf(k.toString())
+        return answer
     }
 }
