c4f715757ba2eaff9988b23a8548820f251e93b2
다을짱 <139089298+ouowinnie@users.noreply.github.com>
2023-08-11 12:11:48 +0900

    [unrated] Title: 공배수, Time: 0.01 ms, Memory: 59.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181936. 공배수/공배수.kt
+++ 프로그래머스/unrated/181936. 공배수/공배수.kt
@@ -1,8 +1,6 @@
 class Solution {
     fun solution(number: Int, n: Int, m: Int): Int {
         var answer: Int = 0
-        
-        if(number % n == 0 && number % m == 0) answer = 1 else answer = 0
-        return answer
+        return if(number % n == 0 && number % m == 0) 1 else 0
     }
 }
