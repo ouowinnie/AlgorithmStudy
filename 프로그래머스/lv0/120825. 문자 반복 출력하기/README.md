4d8db8d91e991576f20c727b0155e639b799cfc8
다을짱 <139089298+ouowinnie@users.noreply.github.com>
2023-09-12 19:25:38 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 11.97 ms, Memory: 60.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120825. 문자 반복 출력하기/README.md
+++ 프로그래머스/lv0/120825. 문자 반복 출력하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 문자 반복 출력하기 - 120825 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120825) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120825?language=kotlin) 
 
 ### 성능 요약
 
-메모리: 65.6 MB, 시간: 37.66 ms
+메모리: 60.5 MB, 시간: 11.97 ms
 
 ### 구분
 
--- 프로그래머스/lv0/120825. 문자 반복 출력하기/문자 반복 출력하기.kt
+++ 프로그래머스/lv0/120825. 문자 반복 출력하기/문자 반복 출력하기.kt
@@ -1,10 +1,12 @@
 class Solution {
     fun solution(my_string: String, n: Int): String {
-        var answer: String = ""
+        var answer: String = "" 
         
-        for(i in 0 until n) {
-            answer += my_string
+        for (i in my_string) {
+            repeat(n) {
+                answer += i
+            }
         }
-        return answer.toCharArray().sortedWith(compareBy { my_string.indexOf(it) }).joinToString("")
+        return answer
     }
 }
